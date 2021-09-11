6d3b0ce92730d2570ac74137146298d41f260c01
Soham <raysoham1@gmail.com>
2021-09-11 14:11:14 -0700

    changed display caption to "Snek Game for SneekOS"

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-
 import pygame
 
 # display code starts here
 pygame.init()
 dis = pygame.display.set_mode((500, 500))
 pygame.display.update()
+pygame.display.set_caption('Snek Game for SneekOS')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
 pygame.quit()
 quit()
 
